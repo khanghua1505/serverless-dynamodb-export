86566fdc07513fff5a7266142876cc0a2420289f
Khang Hua <khanghua1505@gmail.com>
2024-03-28 22:41:41 +0700

    update cloudformation custom resource

--- assets/put-variable/dynamodb.js
+++ assets/put-variable/dynamodb.js
@@ -1,5 +1,4 @@
 const {
-  DescribeTableCommand,
   DeleteItemCommand,
   DynamoDBClient,
   PutItemCommand,
@@ -8,63 +7,20 @@ const {marshall} = require('@aws-sdk/util-dynamodb');
 
 const client = new DynamoDBClient();
 
-async function describeTable(tableName) {
-  const input = new DescribeTableCommand({
-    TableName: tableName,
-  });
-  const response = await client.send(input);
-  if (!response.Table) {
-    throw new Error(`${tableName} resource not found`);
-  }
-  let hashKey;
-  let rangeKey;
-  for (const key of response.Table.KeySchema || []) {
-    if (key.KeyType === 'HASH') {
-      hashKey = key.AttributeName;
-    }
-    if (key.KeyType === 'RANGE') {
-      rangeKey = key.AttributeName;
-    }
-  }
-  for (const attr of response.Table.AttributeDefinitions || []) {
-    if (
-      (attr.AttributeName === hashKey || attr.AttributeName === rangeKey) &&
-      attr.AttributeType !== 'S'
-    ) {
-      throw new Error('hash key and range key must be a string');
-    }
-  }
-  return {
-    tableName: tableName,
-    keySchema: {
-      hashKey,
-      rangeKey,
-    },
-  };
-}
-
-async function putParameter(tableName, name, val) {
-  const tb = await describeTable(tableName);
-  const {hashKey, rangeKey} = tb.keySchema;
+async function putParameter(tableName, item) {
   const input = new PutItemCommand({
     TableName: tableName,
-    Item: marshall({
-      [hashKey]: name,
-      value: val,
-      ...(rangeKey ? {[rangeKey]: name} : {}),
-    }),
+    Item: marshall(item),
   });
   await client.send(input);
 }
 
-async function deleteParameter(tableName, name) {
-  const tb = await describeTable(tableName);
-  const {hashKey, rangeKey} = tb.keySchema;
+async function deleteParameter(tableName, hashKey, rangeKey, item) {
   const input = new DeleteItemCommand({
     TableName: tableName,
     Key: marshall({
-      [hashKey]: name,
-      ...(rangeKey ? {[rangeKey]: name} : {}),
+      [hashKey]: item[hashKey],
+      ...(rangeKey ? {[rangeKey]: item[rangeKey]} : {}),
     }),
   });
   await client.send(input);
--- assets/put-variable/index.js
+++ assets/put-variable/index.js
@@ -2,33 +2,40 @@ const {send, SUCCESS, FAILED} = require('./cfn-response');
 const {putParameter, deleteParameter} = require('./dynamodb');
 
 exports.handler = (event, context) => {
+  handle(event)
+    .then(result => {
+      const {physicalResourceId, status} = result;
+      send(event, context, status, {}, physicalResourceId);
+    })
+    .catch(err => {
+      console.error('error', err);
+      send(event, context, FAILED, {});
+    });
+};
+
+async function handle(event) {
   const {
     TableName: tableName,
-    Name: name,
-    Value: value,
+    HashKey: hashKey,
+    RangeKey: rangeKey,
+    Item: item,
   } = event.ResourceProperties || {};
+  const physicalResourceId = tableName;
   console.log('ResourceProperties ', event.ResourceProperties);
-  if (!tableName || !name || !value) {
-    send(event, context, FAILED);
-    return;
+  if (!tableName || !hashKey || !item) {
+    return {
+      physicalResourceId,
+      status: FAILED,
+    };
   }
-
   if (event.RequestType === 'Create' || event.RequestType === 'Update') {
-    const physicalResourceId = `${tableName}:${name}`;
-    putParameter(tableName, name, value)
-      .then(() => send(event, context, SUCCESS, {}, physicalResourceId))
-      .catch(err => {
-        console.error('error', err);
-        send(event, context, FAILED, {}, physicalResourceId);
-      });
+    await putParameter(tableName, item);
   }
   if (event.RequestType === 'Delete') {
-    const physicalResourceId = `${tableName}:${name}`;
-    deleteParameter(tableName, name, value)
-      .then(() => send(event, context, SUCCESS, {}, physicalResourceId))
-      .catch(err => {
-        console.error('error', err);
-        send(event, context, FAILED, {}, physicalResourceId);
-      });
+    await deleteParameter(tableName, hashKey, rangeKey, item);
   }
-};
+  return {
+    physicalResourceId,
+    status: SUCCESS,
+  };
+}
